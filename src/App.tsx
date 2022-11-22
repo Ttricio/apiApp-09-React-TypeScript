3aa39347e82fa6d6142dcd6e283eb263a2d79718
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-11-22 15:08:04 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import classes from "./style.module.scss"
+
+
 
 function App() {
+  const [word,setWord] = useState("")
+  const fetchHandler=()=>{
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <div className={classes.top__section}>
+
+   <div className={classes.title}>
+    <h1>Synonims app</h1>
+   </div>
+   <form className={classes.search} onSubmit={fetchHandler}>
+    <label htmlFor="search">Your word </label>
+    <input type="text" name="search"placeholder="search synonims ..." onChange={(e)=>setWord(e.target.value)}/>
+
+    <button>Submit</button>
+
+   
+   </form>
+      </div>
+      <div className={classes.bottom__section}
+      >
+<div className={classes.card}>
+  <div className={classes.card}>
+    <p className={classes.display}>new</p>
+  </div>
+</div>
+
+      </div>
     </div>
   );
 }
