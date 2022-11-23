cab26ae3fb7946489b2e60a15c972b2dea419d78
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-11-23 09:34:50 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,61 @@
-import React, { useState } from 'react';
-import classes from "./style.module.scss"
+import React, { useState } from "react";
+import classes from "./style.module.scss";
 
+type Synonym = {
+	word: string;
+	score: number;
+};
 
+const URL = ` https://api.datamuse.com`;
 
 function App() {
-  const [word,setWord] = useState("")
-  const fetchHandler=()=>{
+	const [word, setWord] = useState("");
+	const [synonyms, setSynonyms] = useState<Synonym[]>([]);
 
-  }
-  return (
-    <div className={classes.container}>
-      <div className={classes.top__section}>
+	const fetchSynonyms = (word: string) => {
+		fetch(` ${URL}/words?rel_syn=${word}`)
+			.then((response) => response.json())
+			.then(setSynonyms);
+	};
 
-   <div className={classes.title}>
-    <h1>Synonims app</h1>
-   </div>
-   <form className={classes.search} onSubmit={fetchHandler}>
-    <label htmlFor="search">Your word </label>
-    <input type="text" name="search"placeholder="search synonims ..." onChange={(e)=>setWord(e.target.value)}/>
+	const fetchHandler = (e: React.FormEvent) => {
+		e.preventDefault();
+		
+		fetchSynonyms(word);
+	};
 
-    <button>Submit</button>
+	const synonymHandler = (newWord: string) => {
+    fetchSynonyms(newWord);
+    setWord(newWord);
+	};
+	return (
+		<div className={classes.container}>
+			<div className={classes.top__section}>
+				<div className={classes.title}>
+					<h1>Synonyms app</h1>
+				</div>
+				<form className={classes.search} onSubmit={fetchHandler}>
+					<label htmlFor='search'>Your word </label>
+					<input
+						type='text'
+						name='search'
+            value={word}
+						placeholder='search synonyms ...'
+						onChange={(e) => setWord(e.target.value)}
+					/>
 
-   
-   </form>
-      </div>
-      <div className={classes.bottom__section}
-      >
-<div className={classes.card}>
-  <div className={classes.card}>
-    <p className={classes.display}>new</p>
-  </div>
-</div>
-
-      </div>
-    </div>
-  );
+					<button>Submit</button>
+				</form>
+			</div>
+			<div className={classes.bottom__section}>
+				{synonyms.map((synonym) => (
+					<div className={classes.synonyms__list} key={synonym.word} onClick={() => synonymHandler(synonym.word)}>
+						<li >{synonym.word}</li>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
